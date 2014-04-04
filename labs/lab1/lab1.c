be6e6f157356ac16d2e0a18d7595d34960b527ef
Sean Penney <penneys@os-class.engr.oregonstate.edu>
2014-04-04 14:43:51 -0700

    Finished frexp

--- labs/lab1/lab1.c
+++ labs/lab1/lab1.c
@@ -3,16 +3,44 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int
-main(int argc, char *argv[])
+int main(int argc, char *argv[])
 {
-    double x, r;
-    int exp;
+   double x, r;
+   int exp;
 
    x = strtod(argv[1], NULL);
-    r = frexp(x, &exp);
+   r = frexp(x, &exp);
 
    printf("frexp(%g, &e) = %g: %g * %d^%d = %g\n",
-           x, r, r, FLT_RADIX, exp, x);
-    exit(EXIT_SUCCESS);
+			x, r, r, FLT_RADIX, exp, x);
+   exit(EXIT_SUCCESS);
+}
+
+double frexp(double x, int *exp) {
+	int exponent = 2;
+	int counter = 1;
+	double fraction;
+	
+	if (x == NAN) {
+		return NAN;
+	}
+	if (x == INFINITY) {
+		return INFINITY;
+	}
+	if (x == -INFINITY) {
+		return -INFINITY;
+	}
+	if (x == 0) {
+		*exp = 0;
+		return 0;
+	}
+	
+	while (exponent < x) {
+		exponent = 2*exponent;
+		counter++;
+	}
+	*exp = counter;
+	
+	fraction = x / exponent;
+	return fraction;
 }
