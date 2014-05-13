168a41b6a0f72994bceb858975672d943a6722dc
Paul Atkinson <atkinspa@flip1.engr.oregonstate.edu>
2014-05-13 16:51:49 -0700

    added lab 4

--- labs/lab2/lab2_palindromes.c
+++ labs/lab2/lab2_palindromes.c
@@ -2,18 +2,17 @@
 #include <float.h> 
 #include <stdio.h>
 #include <stdlib.h>
-
+#include <string.h>
 
 int main(int argc, char *argv[]) {
-	char* word = argv[1];
-	int halfsize = (sizeof(word)/2)
-	int true = 1;	
+	char* var_word = argv[1];
+	int halfsize = (strlen(var_word)/2);
+	int true = 1;
 
-
-	//check if word is odd
-	if((sizeof(word)%2) == 1) {
+	//check if var_word is odd
+	if((strlen(var_word)%2) == 1) {
 		for(int i = 0; i < halfsize; i++) {
-			if(word[i] != word[sizeof(word)-i]) {
+			if(var_word[i] != var_word[strlen(var_word)-i -1]) {
 				true = 0;
 			}
 		}
